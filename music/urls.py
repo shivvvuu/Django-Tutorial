36b895a793b091759b5f046513ee6b699ac7710d
shivam <jhodshivam5122@gmail.com>
2023-04-08 12:04:57 +0530

    Namespacing and HTTP404 shortcut

--- music/urls.py
+++ music/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, re_path
 from . import views
 
-
-
+app_name = 'music'  # It's the name of the app diff app have diff name and for example if two or move have same url ass detail so how django will know which detail page to open name: detail have same in other apps too
 
 urlpatterns = [
     # /music/
--- music/views.py
+++ music/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
-from django.http import Http404
+# from django.http import Http404
 
 def index(request):
     all_album = Album.objects.all()
@@ -10,8 +10,9 @@ def index(request):
     return render(request, 'music/index.html', context )
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk = album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album Does not exist")
+    # try:
+    #     album = Album.objects.get(pk = album_id)
+    # except Album.DoesNotExist:
+    #     raise Http404("Album Does not exist")
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
