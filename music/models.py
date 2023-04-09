21564a398f8df8ec09d7ac7d38b3510f39cabaff
shivam <jhodshivam5122@gmail.com>
2023-04-09 22:17:32 +0530

    delete and update

--- music/models.py
+++ music/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -7,6 +8,11 @@ class Album(models.Model):
     alum_title =models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
     alum_logo = models.CharField(max_length=1000)
+    
+    def get_absolute_url(self):
+        return reverse("music:detail", kwargs={"pk": self.pk})
+    
+    
     
     def __str__(self) :
         return self.alum_title + '-' + self.artist
--- music/views.py
+++ music/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 from .models import Album
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
 class IndexView(generic.ListView):
@@ -14,9 +16,17 @@ class DetailView(generic.DetailView):
     model =Album
     template_name = 'music/detail.html'
     
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist', 'alum_title', 'genre', 'alum_logo']
+    
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'alum_title', 'genre', 'alum_logo']
 
-
-
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
 
 
 
